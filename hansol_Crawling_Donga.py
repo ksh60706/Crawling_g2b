648ef67d58ff1002317908adf609d6158cc203da
SHx3 <ksh60706@gmail.com>
2019-07-23 11:09:58 +0900

    형태소 분석 추가

--- hansol_Crawling_Donga.py
+++ hansol_Crawling_Donga.py
@@ -6,6 +6,8 @@ from elasticsearch import Elasticsearch
 import json
 import datetime
 
+from konlpy.tag import Kkma
+
 # 어제 날짜 구하기
 YESTERDAY_DATE = (datetime.date.today() - datetime.timedelta(1)).strftime("%Y%m%d")
 
@@ -23,7 +25,7 @@ MONGO_USER_PASSWORD = "Hanhdwas200!"
 # 검색 키워드
 target_keyword = "한솔제지"
 
-
+kkma = Kkma()
 
 
 # 정규화 처리
@@ -90,11 +92,9 @@ def get_detail_url_from_list(URL):
     :param URL: 검색결과 url
     :return: 상세 페이지의 URL
     '''
-    #print("HELLO")
 
     try:
-        #print("TRY")
-        #print(URL)
+
         html = requests.get(URL).text
         soup = BeautifulSoup(html, "html.parser")
 
@@ -112,18 +112,17 @@ def get_content_from_link(URL):
     :param URL: 상세페이지 url
     :return:  크롤링 내용
     '''
-    #print("HELLO2")
-    #print(URL)
+
     content = ""
+    #f = open("C:\\Users\KSH\PycharmProjects\Crawling_g2b\형태소분석.txt", "w")
+    #f.write("시작 : ")
     try:
         html = requests.get(URL).text
         soup = BeautifulSoup(html, "html.parser")
 
         title = soup.select_one("div.article_title > h1.title").text
 
-        #content = soup.select_one("div.article_txt").text
-        #content_tag = soup.select("div.article_txt")
-        #print(content_tag)
+
         '''
         for item in content_tag:
             string_item = str(item.find_all(text=True))
@@ -132,12 +131,18 @@ def get_content_from_link(URL):
         print("콘텐츠는 : ",content)
         '''
 
-        content = normalize_spaces(soup.select_one("div.article_txt").text).split(" ")
-        #print(content)
+
+        #for i in kkma.pos(normalize_spaces(soup.select_one("div.article_txt").text)):
+        #for i in kkma.nouns(normalize_spaces(soup.select_one("div.article_txt").text)):
+        #    f.write(str(i)+"\n")
+        #f.write(kkma.pos(normalize_spaces(soup.select_one("div.article_txt").text)))
+
+        #content = normalize_spaces(soup.select_one("div.article_txt").text).split(" ")
+        content = kkma.nouns(normalize_spaces(soup.select_one("div.article_txt").text))
 
         newsDate_tag = soup.select("div.title_foot > span.date01")[0].text
-        #print(newsDate_tag)
-        newsDate = newsDate_tag.replace("입력 ","")
+        newsDate = datetime.datetime.strptime( newsDate_tag.replace("입력 ","")+":00", "%Y-%m-%d %H:%M:%S")
+
         news = {
             "crawling_type": "뉴스",
             "crawling_company": "동아일보",
@@ -147,6 +152,7 @@ def get_content_from_link(URL):
             "crawling_content": content,
             "crawling_newsDate": newsDate
         }
+        #f.close()
         return news
 
     except Exception as ex:
@@ -156,16 +162,17 @@ def get_content_from_link(URL):
 # 메인 함수
 def main():
 
+
     # 페이지 수 확인
     TARGET_URL = TARGET_URL_BEFORE_KEYWORD + TARGET_URL_KEYWORD + target_keyword
     target_pageNo = check_page_count(TARGET_URL)
     #target_pageNo = 1
 
-    TARGET_URL = ""
+    #TARGET_URL = ""
 
     # 디비연결
     db = db_conn()
-    collection = db.NEWS_HANSOL
+    collection = db.NEWS_HANSOL_WORDS
 
     # 엘라스틱서치 연결
     es = es_conn()
@@ -176,7 +183,7 @@ def main():
 
         TARGET_URL = TARGET_URL_BEFORE_KEYWORD + TARGET_URL_KEYWORD + target_keyword + TARGET_URL_PAGE_NO + str(15*i + 1)
 
-        #print(TARGET_URL)
+        print(TARGET_URL)
         urls = get_detail_url_from_list(TARGET_URL)
 
         for url in urls:
@@ -184,12 +191,13 @@ def main():
 
             # 엘라스틱 데이터 입력
             content_json = json.dumps(content)
-            es.index(index="crawling_testtt", body=content_json, id=content["crawling_url"].split("/all/")[1])
+            es.index(index="crawling_testtt_words", body=content_json, id=content["crawling_url"].split("/all/")[1].replace('/','-'))
 
             # 디비 데이터 입력
-            #print("데이터 입력 전 ")
             collection.insert_one(content)
-            #print("데이터 입력 후 ")
+
+            # 형태소 분석
+            #print(kkma.pos(content[]))
 
 
 
